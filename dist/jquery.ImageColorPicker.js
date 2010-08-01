56e5585a2e4194e2f08cee423dfacf6a62356b80
Stefan Siebel <siebel.stefan@gmail.com>
2010-08-01 23:34:57 +0200

    converted plugin to ui widget.

--- dist/jquery.ImageColorPicker.js
+++ dist/jquery.ImageColorPicker.js
@@ -8,104 +8,152 @@
 * 
 * Released under the MIT
 *
-* Date: Wed Jul 28 21:29:41 2010 +0200
+* Date: Sun Aug 1 19:39:38 2010 +0200
 */
-(function($) {
 
-  $.fn.ImageColorPicker = function(settings) {
+$.widget("ui.ImageColorPicker", {
+	// default options
+	options: {
 
-    // add default options here
-    var config = {};
+	},
+	_create: function() {
+		if (this.element.get(0).tagName.toLowerCase() === 'img') {
+      if (this.element.get(0).complete) {
+				this._createImageColorPicker();
+      } else {
+				this.element.bind('load', { that: this }, function(e){
+					var that = e.data.that;
+					that._createImageColorPicker();
+				});
+			}
+		}
+		
+	},
+	
+	destroy: function() {
+		// default destroy		
+		$.Widget.prototype.destroy.apply(this, arguments);
+		
+		// remove possible large array with pixel data
+		this.imageData = null;
+		
+		// remove additional elements
+		this.$currentColor.remove();
+		this.$selectedColor.remove();
+		this.$canvas.remove();
+		this.$toolbox.remove();
+		this.element.unwrap();
+		this.element.show();
+	},
+	
+	selectedColor: function() {
+		var color = this.$selectedColor.css("backgroundColor");
+		var red = 255;
+		var green = 255;
+		varblue = 255;		
+		var rgbvals = null;
+		
+		if (color.substring(0, 1) === '#') {
+			red = this._h2d(color.substring(0,2));
+			green = this._h2d(color.substring(2, 2));
+			blue = this._h2d(color.substring(4, 2));
+		} else if (color.substring(0, 4) === 'rgba') {
+			rgbvals = /rgba\((.+),(.+),(.+)\)/i.exec(color); 
+			red = rgbvals[1].trim();
+			green = rgbvals[2].trim();
+			blue = rgbvals[3].trim();
+		} else if (color.substring(0, 3) === 'rgb') {
+			rgbvals = /rgb\((.+),(.+),(.+)\)/i.exec(color); 
+			red = rgbvals[1].trim();
+			green = rgbvals[2].trim();
+			blue = rgbvals[3].trim();
+		} 
 
-    var createImageColorPicker = function(that) {         
-      var $this = $(that);
-      var $currentColor = $('<div class="currentColor"></div>');
-      var $selectedColor = $('<div class="selectedColor"></div>');
-      var $canvas = $('<canvas class="ImageColorPickerCanvas"></canvas>');
-      var $toolbox = $('<div class="ImageColorPickerToolBox"></div>');
-      
-      
-      $this.wrap('<div class="ImageColorPickerWrapper"></div>');
-      
-      var $wrapper = $this.parent();
-      
-      $wrapper.append($canvas);
-      $wrapper.append($toolbox);
-      $toolbox.append($currentColor);
-      $toolbox.append($selectedColor);
-      
-      var ctx = $canvas.get(0).getContext('2d');
+		return "#" + this._d2h(red) + this._d2h(green) + this._d2h(blue);
+	},
+	
+	_d2h: function(d) {
+		if (parseInt(d)) {
+			return parseInt(d).toString(16);
+		} else {
+			return d;
+		}
+	},
+	
+	_h2d: function(h) {
+		return parseInt(h,16);
+	},
+   
+	_createImageColorPicker: function() {
+	
+		// create additional DOM elements.
+    this.$currentColor = $('<div class="currentColor"></div>');
+    this.$selectedColor = $('<div class="selectedColor"></div>');
+    this.$canvas = $('<canvas class="ImageColorPickerCanvas"></canvas>');
+    this.$toolbox = $('<div class="ImageColorPickerToolBox"></div>');
 
-		  var img = new Image();
-		  img.onload = function(){  
-		    ctx.drawImage(img, 0, 0); 
+		// add them to the DOM
+    this.element.wrap('<div class="ImageColorPickerWrapper"></div>');
+    this.$wrapper = this.element.parent();
+    this.$wrapper.append(this.$canvas);
+    this.$wrapper.append(this.$toolbox);
+    this.$toolbox.append(this.$currentColor);
+    this.$toolbox.append(this.$selectedColor);
+    
+    // get canvas context.
+    var ctx = this.$canvas.get(0).getContext('2d');
 
-        try {
-					try { 
-            $canvas.data("ImageData", ctx.getImageData(0, 0, img.width, img.height));  
-          } catch (e1) { 
-            netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
-            $canvas.data("ImageData", ctx.getImageData(0, 0, img.width, img.height)); 
-					}
-        } catch (e2) {
-          throw new Error("unable to access image data: " + e);
-        } 
-      };
-      img.src = $this.attr("src");
-      
-      $canvas.attr("width", img.width);
-      $canvas.attr("height", img.height);
-      
-      // hide the original image
-      $this.hide();
-      
-      $canvas.bind("mousemove", function(e){
-        
-		    var offset = $canvas.offset();
-		    var x = e.pageX - offset.left;
-		    var y = e.pageY - offset.top;
-				var pixel = ((y * img.width) + x) * 4;
-				var imageData = $canvas.data("ImageData");
-        $currentColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
-      }); 
-      
-      $canvas.bind("click", function(e){
-        
-		    var offset = $canvas.offset();
-		    var x = e.pageX - offset.left;
-		    var y = e.pageY - offset.top;
-				var pixel = ((y * img.width) + x) * 4;
-				var imageData = $canvas.data("ImageData");
-        $selectedColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
-      }); 
-      
-      $canvas.bind("mouseleave", function(e){
-        $currentColor.css("backgroundColor", "#fff");
-      });
-    };
-  
-    if (settings) {
-			$.extend(config, settings);
-    }
-      
-    this.each(function() {
-        var that = this;
-        
-        // some magic to make sure the image is really loaded.
-        if (that.tagName.toLowerCase() === 'img') {
-          if (that.completed) {
-            createImageColorPicker(that);
-          } else {
-            $(that).load(function(){
-              createImageColorPicker(that);            
-            });
-          }
-        }
-        
+		// draw the image in the canvas
+	  var img = new Image();
+	  img.src = this.element.attr("src");
+		this.$canvas.attr("width", img.width);
+		this.$canvas.attr("height", img.height);
+		ctx.drawImage(img, 0, 0); 
+
+		// get the image data.
+    try {
+			try { 
+        this.imageData = ctx.getImageData(0, 0, img.width, img.height);  
+      } catch (e1) { 
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
+        this.imageData = ctx.getImageData(0, 0, img.width, img.height);  
+			}
+    } catch (e2) {
+      throw new Error("unable to access image data: " + e);
+    } 
+    
+    // hide the original image
+    this.element.hide();
+    
+    // for usage in events
+    var that = this;
+    
+    this.$canvas.bind("mousemove", function(e){
+	    var offset = that.$canvas.offset();
+	    var x = e.pageX - offset.left;
+	    var y = e.pageY - offset.top;
+			var pixel = ((y * img.width) + x) * 4;
+			var imageData = that.imageData;
+      that.$currentColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
+    }); 
+    
+    this.$canvas.bind("click", function(e){
+	    var offset = that.$canvas.offset();
+	    var x = e.pageX - offset.left;
+	    var y = e.pageY - offset.top;
+			var pixel = ((y * img.width) + x) * 4;
+			var imageData = that.imageData;
+      that.$selectedColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
+    }); 
+    
+    this.$canvas.bind("mouseleave", function(e){
+      that.$currentColor.css("backgroundColor", "#fff");
     });
-  
-    return this;
+    
+    // hope that helps to prevent memory leaks
+    $(window).unload(function(e){
+    	that.destroy();
+    });
+	}
+});
 
-  };
-
-})(jQuery);
