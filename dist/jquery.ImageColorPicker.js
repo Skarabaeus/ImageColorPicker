6e9a986c70e7c167d2b4fef7272dd9eb9073b59d
Stefan Siebel <siebel.stefan@gmail.com>
2010-08-11 21:49:20 +0200

    adding excanvas support

--- dist/jquery.ImageColorPicker.js
+++ dist/jquery.ImageColorPicker.js
@@ -1,5 +1,5 @@
 /*!
-* jQuery ImageColorPicker Plugin v0.1
+* jQuery ImageColorPicker Plugin v0.2
 * http://github.com/Skarabaeus/ImageColorPicker
 *
 * Copyright 2010, Stefan Siebel
@@ -8,7 +8,7 @@
 * 
 * Released under the MIT
 *
-* Date: Sat Aug 7 18:29:34 2010 +0200
+* Date: Sun Aug 8 19:11:26 2010 +0200
 */
 
 $.widget("ui.ImageColorPicker", {
@@ -18,9 +18,9 @@ $.widget("ui.ImageColorPicker", {
 	},
 	_create: function() {
 		if (this.element.get(0).tagName.toLowerCase() === 'img') {
-      if (this.element.get(0).complete) {
+			if (this.element.get(0).complete) {
 				this._createImageColorPicker();
-      } else {
+			} else {
 				this.element.bind('load', { that: this }, function(e){
 					var that = e.data.that;
 					that._createImageColorPicker();
@@ -89,83 +89,94 @@ $.widget("ui.ImageColorPicker", {
 	_h2d: function(h) {
 		return parseInt(h,16);
 	},
-   
+	 
 	_createImageColorPicker: function() {
 	
 		// create additional DOM elements.
-    this.$currentColor = $('<div class="currentColor"></div>');
-    this.$selectedColor = $('<div class="selectedColor"></div>');
-    this.$canvas = $('<canvas class="ImageColorPickerCanvas"></canvas>');
-    this.$toolbox = $('<div class="ImageColorPickerToolBox"></div>');
+		this.$currentColor = $('<div class="currentColor"></div>');
+		this.$selectedColor = $('<div class="selectedColor"></div>');
+		this.$canvas = $('<canvas class="ImageColorPickerCanvas"></canvas>');
+		this.$toolbox = $('<div class="ImageColorPickerToolBox"></div>');
 
 		// add them to the DOM
-    this.element.wrap('<div class="ImageColorPickerWrapper"></div>');
-    this.$wrapper = this.element.parent();
-    this.$wrapper.append(this.$canvas);
-    this.$wrapper.append(this.$toolbox);
-    this.$toolbox.append(this.$currentColor);
-    this.$toolbox.append(this.$selectedColor);
-    
-    // get canvas context.
-    var ctx = this.$canvas.get(0).getContext('2d');
+		this.element.wrap('<div class="ImageColorPickerWrapper"></div>');
+		this.$wrapper = this.element.parent();
+		this.$wrapper.append(this.$canvas);
+		this.$wrapper.append(this.$toolbox);
+		this.$toolbox.append(this.$currentColor);
+		this.$toolbox.append(this.$selectedColor);
+		
+		// try to get canvas context.
+		var ctx = null;
+		
+		if (typeof(this.$canvas.get(0).getContext) === 'function') { // FF, Chrome, ...
+			ctx = this.$canvas.get(0).getContext('2d');
+		} else if (typeof(G_vmlCanvasManager) !== 'undefined') { // IE, with excanvas
+			G_vmlCanvasManager.initElement(this.$canvas.get(0));
+			var ctx = this.$canvas.get(0).getContext('2d');
+		} else {
+			this.destroy();
+			throw new Error("ImageColor Picker: Can't get canvas context. Use "
+				+ "Firefox, Chrome or include excanvas to your project.");
+		}
 
 		// draw the image in the canvas
-	  var img = new Image();
-	  img.src = this.element.attr("src");
+		var img = new Image();
+		img.src = this.element.attr("src");
 		this.$canvas.attr("width", img.width);
 		this.$canvas.attr("height", img.height);
 		ctx.drawImage(img, 0, 0); 
 
 		// get the image data.
-    try {
+		try {
 			try { 
-        this.imageData = ctx.getImageData(0, 0, img.width, img.height);  
-      } catch (e1) { 
-        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
-        this.imageData = ctx.getImageData(0, 0, img.width, img.height);  
+				this.imageData = ctx.getImageData(0, 0, img.width, img.height);	
+			} catch (e1) { 
+				netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
+				this.imageData = ctx.getImageData(0, 0, img.width, img.height);	
 			}
-    } catch (e2) {
-    	this.destroy();
-      throw new Error("ImageColor Picker: Unable to access image data. "
-      	+ "This could be either due " 
-      	+ "to the browser you are using (IE doesn't work) or image and script "
-      	+ "are saved on different servers or you run the script locally. "
-      	+ "The exception is: " + e2);
-    } 
-    
-    // hide the original image
-    this.element.hide();
-    
-    // for usage in events
-    var that = this;
-    
-    this.$canvas.bind("mousemove", function(e){
-	    var offset = that.$canvas.offset();
-	    var x = e.pageX - offset.left;
-	    var y = e.pageY - offset.top;
+		} catch (e2) {
+			this.destroy();
+			throw new Error("ImageColor Picker: Unable to access image data. "
+				+ "This could be either due " 
+				+ "to the browser you are using (IE doesn't work) or image and script "
+				+ "are saved on different servers or you run the script locally. "
+				+ "The exception is: " + e2);
+		} 
+		
+		// hide the original image
+		this.element.hide();
+		
+		// for usage in events
+		var that = this;
+		
+		this.$canvas.bind("mousemove", function(e){
+			var offset = that.$canvas.offset();
+			var x = e.pageX - offset.left;
+			var y = e.pageY - offset.top;
 			var pixel = ((y * img.width) + x) * 4;
 			var imageData = that.imageData;
-      that.$currentColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
-    }); 
-    
-    this.$canvas.bind("click", function(e){
-	    var offset = that.$canvas.offset();
-	    var x = e.pageX - offset.left;
-	    var y = e.pageY - offset.top;
+			that.$currentColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
+		}); 
+		
+		this.$canvas.bind("click", function(e){
+			var offset = that.$canvas.offset();
+			var x = e.pageX - offset.left;
+			var y = e.pageY - offset.top;
 			var pixel = ((y * img.width) + x) * 4;
 			var imageData = that.imageData;
-      that.$selectedColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
-      that._trigger("afterColorSelected", 0, that.selectedColor());
-    }); 
-    
-    this.$canvas.bind("mouseleave", function(e){
-      that.$currentColor.css("backgroundColor", "#fff");
-    });
-    
-    // hope that helps to prevent memory leaks
-    $(window).unload(function(e){
-    	that.destroy();
-    });
+			that.$selectedColor.css("backgroundColor", "rgba(" + imageData.data[pixel] + ", " + imageData.data[(pixel + 1)] + ", " + imageData.data[(pixel + 2)] + ", " + imageData.data[(pixel + 3)] + ")");
+			that._trigger("afterColorSelected", 0, that.selectedColor());
+		}); 
+		
+		this.$canvas.bind("mouseleave", function(e){
+			that.$currentColor.css("backgroundColor", "#fff");
+		});
+		
+		// hope that helps to prevent memory leaks
+		$(window).unload(function(e){
+			that.destroy();
+		});
 	}
 });
 
